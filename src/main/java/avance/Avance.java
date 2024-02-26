29a65acbf2714f4fd63dd18778e442cd11cf07b4
AdryHD <152119270+AdryHD@users.noreply.github.com>
2024-02-25 18:15:28 -0600

    ok

--- src/main/java/avance/Avance.java
+++ src/main/java/avance/Avance.java
@@ -67,7 +67,6 @@ public class Avance {
                 default:
                     JOptionPane.showMessageDialog(null, opcionMenu + " Este número no es una opcion válida.");
                     break;
-
             }
         }
      }
